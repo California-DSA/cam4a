dbbe9a5333a8ebdbacdba8a1b78e86c5bca54e31
Vivian Brown <vivian@netlify.com>
2021-03-31 20:32:03 -0700

    Add javascript

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,9 +3,8 @@
     <div class="w3-bar w3-red w3-card w3-left-align w3-large">
       <a
         class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-red"
-        href="javascript:void(0);"
-        onclick="myFunction()"
         title="Toggle Navigation Menu"
+        @click="toggleMenu"
         ><i class="fa fa-bars"></i
       ></a>
       <a href="#" class="w3-bar-item w3-button w3-padding-large w3-white"
@@ -45,3 +44,19 @@
     </div>
   </div>
 </template>
+
+<script>
+// Used to toggle the menu on small screens when clicking on the menu button
+export default {
+  methods: {
+    toggleMenu() {
+      const x = document.getElementById('navDemo')
+      if (!x.className.includes('w3-show')) {
+        x.className += ' w3-show'
+      } else {
+        x.className = x.className.replace(' w3-show', '')
+      }
+    },
+  },
+}
+</script>
