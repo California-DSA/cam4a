d86431de01e51abbf6805e8a93a867fd39f5ee23
Vivian Brown <vivian@netlify.com>
2021-03-31 19:20:22 -0700

    Create index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,156 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">cam4a</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <!-- Intro -->
+  <div id="main">
+    <div id="faq" class="w3-row-padding w3-padding-32 w3-container">
+      <div class="w3-content">
+        <div class="w3-twothird">
+          <h1>It's time for single-payer in California.</h1>
+          <h5 class="w3-padding-32" style="padding-left: 0px !important">
+            The US healthcare system is designed to maximize profit, not patient
+            care. Millions of working-class Americans have lost their health
+            insurance during the COVID-19 pandemic; meanwhile, California's 165
+            billionaires have seen their wealth increase by $175,000,000,000
+            since March. In the richest state in the richest country in the
+            world, this is unacceptable. We can and we must pass Medicare for
+            All—and that starts here in California.
+          </h5>
+        </div>
+
+        <div id="icons" class="w3-third w3-center">
+          <i class="fa w3-text-red">
+            <img
+              style="width: 100%; object-fit: contain"
+              src="/graphics/singlepayer-cam4asite.png"
+            />
+          </i>
+        </div>
       </div>
+    </div>
+
+    <!-- AB1400 -->
+    <div class="w3-row-padding w3-light-grey w3-padding-32 w3-container">
+      <div class="w3-content">
+        <div id="icons" class="w3-third w3-center">
+          <i class="fa w3-text-red w3-margin-right">
+            <img
+              style="width: 100%; object-fit: contain"
+              src="/graphics/dsa-cam4asite.png"
+            />
+          </i>
+        </div>
+
+        <div class="w3-twothird">
+          <h1>What is AB1400?</h1>
+          <h5 class="w3-padding-32" style="padding-left: 0px !important">
+            Assembly Bill 1400 (AB1400) would create the California Guaranteed
+            Health Care for All program, or CalCare, to provide comprehensive
+            universal single-payer health care coverage for the benefit of all
+            residents of the state. Authored by State Assembly member Ash Kalra,
+            AB1400 would expand coverage to nearly 3 million uninsired
+            Californians and provide rich benefits, including dental care,
+            generous prescription drug coverage and long-term care.
+          </h5>
+        </div>
+      </div>
+    </div>
+
+    <!-- DSA -->
+    <div class="w3-row-padding w3-padding-32 w3-container">
+      <div class="w3-content">
+        <div class="w3-twothird">
+          <h1>Who are the Democratic Socialists of America?</h1>
+          <h5 class="w3-padding-32" style="padding-left: 0px !important">
+            The Democratic Socialists of America is the largest socialist
+            organization in the United States, with over 92,000 members and
+            chapters in all 50 states. We believe that working people should run
+            both the economy and society democratically to meet human needs, not
+            to make profits for a few. We are a political and activist
+            organization, not a party; through campus and community-based
+            chapters, DSA members use a variety of tactics, from legislative to
+            direct action, to fight for reforms that empower working people.
+          </h5>
+        </div>
+
+        <div id="icons" class="w3-third w3-center">
+          <i class="fa w3-text-red">
+            <img
+              style="width: 100%; object-fit: contain"
+              src="/graphics/dsaLogo.png"
+            />
+          </i>
+        </div>
+      </div>
+    </div>
+
+    <!-- Get Involved -->
+    <div
+      id="volunteer"
+      class="w3-row-padding w3-light-grey w3-padding-32 w3-container"
+    >
+      <div class="w3-content">
+        <div id="icons" class="w3-third w3-center">
+          <i class="fa w3-text-red w3-margin-right">
+            <img
+              style="width: 100%; object-fit: contain"
+              src="/graphics/clipboard.png"
+            />
+          </i>
+        </div>
+
+        <div class="w3-twothird">
+          <h1>Get Involved</h1>
+          <h5 class="w3-padding-32" style="padding-left: 0px !important">
+            Ready to join the fight for CalCare? Get plugged into our campaign
+            here!
+          </h5>
+
+          <table>
+            <tr>
+              <td>
+                <a
+                  href="https://actionnetwork.org/forms/join-our-statewide-campaign-to-win-single-payer-in-california/"
+                  target="_blank"
+                >
+                  <img
+                    style="
+                      width: 80%;
+                      object-fit: contain;
+                      display: block;
+                      margin-left: auto;
+                      margin-right: auto;
+                    "
+                    src="/graphics/b-vol.png"
+                  />
+                </a>
+              </td>
+              <td>
+                <a
+                  href="https://secure.actblue.com/donate/cam4a"
+                  target="_blank"
+                >
+                  <img
+                    style="
+                      width: 80%;
+                      object-fit: contain;
+                      display: block;
+                      margin-left: auto;
+                      margin-right: auto;
+                    "
+                    src="/graphics/b-donate.png"
+                  />
+                </a>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <div class="w3-container w3-center">
+      <img
+        style="width: 100%; object-fit: contain"
+        src="/graphics/footer-cam4asite.png"
+      />
     </div>
   </div>
 </template>
@@ -29,35 +159,4 @@
 export default {}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
